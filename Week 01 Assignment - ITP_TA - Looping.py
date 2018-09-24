d21634311d2e4ea713ca6cbdd098d4a0e7d0a8c7
ryanrusli1407 <43331671+ryanrusli1407@users.noreply.github.com>
2018-09-24 15:25:22 +0700

    Update Week 01 Assignment - ITP_TA - Looping.py

--- Week 01 Assignment - ITP_TA - Looping.py
+++ Week 01 Assignment - ITP_TA - Looping.py
@@ -1,32 +1,30 @@
-for i in range(1,6):
+for i in range(1,6):                #i is a loop for the number of rows
     print("*"*i)                    #normal right angle triangle
 print("\n")
 
 
-for i in range(1,6):
-    print(" "*(6-i), "*"*i)        #mirrored triangle
+for i in range(1,6):                #i is a loop for the number of rows
+    print(" "*(6-i), "*"*i)         #mirrored triangle
 print("\n")
 
 
-for i in range(5,0,-1):
+for i in range(5,0,-1):             #i is a loop for the number of rows
     print("*"*i)                    #upside down right angle triangle
 print("\n")
 
 
 s = 0                               #s is a counter for the number of spaces
-for i in range(1,6):    
+for i in range(1,6):                #i is a loop for the number of rows
     print((s)*(" "), "*"*(6-i))     #mirrored upside down triangle
     s += 1              
 print("\n")
 
-for i in range(1,6):
-    print(" "*(6-i), "*"*(i*2-1))  #full triangle/pyramid
+for i in range(1,6):                #i is a loop for the number of rows
+    print(" "*(6-i), "*"*(i*2-1))   #full triangle/pyramid
 print("\n")
 
-for i in range(1,6):
+for i in range(1,6):                #i is a loop for the number of rows of the upwards triangle
     print(" "*(5-i), "*"*(i*2-1))   #this creates the upwards triangle
-for q in range(1,5):
-    print(" "*(q), "*"*(2*(5-q)-1))  #this creates the downwards triangle
+for q in range(1,5):                #q is a loop for the number of rows of the downwards triangle
+    print(" "*(q), "*"*(2*(5-q)-1)) #this creates the downwards triangle
                                     #diamond
-
-
