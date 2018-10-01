ce2b501925e17a363ca7da8f0cf0eb1d7e71aefc
ryanrusli1407 <43331671+ryanrusli1407@users.noreply.github.com>
2018-10-01 23:46:33 +0700

    Update Word_Counter_with_matplotlib.py

--- Word_Counter_with_matplotlib.py
+++ Word_Counter_with_matplotlib.py
@@ -5,8 +5,6 @@ from collections import OrderedDict
 sortedvalues = []
 sortedkeys = []  
 
-maxi = 0
-
 x = open("Robinhood.txt","r")   #opens the Robinhood text to be read
 x = x.read()        #copies the file contents and inputs it in x
 y = []
@@ -33,19 +31,19 @@ for word in y:                  #for each word in the array y
         wordlist[word] = 1#if not, sets the word count to one and adds the word to the dictionary
 
 
-wordlist = OrderedDict(sorted(wordlist.items(), key=itemgetter(1)))
+wordlist = OrderedDict(sorted(wordlist.items(), key=itemgetter(1))) #orders the values of the dictionary in ascending order
 
-for item in wordlist.values():
-    sortedvalues.append(item)
-for item in wordlist.keys():
-    sortedkeys.append(item)
+for item in wordlist.values():     #loops through every single value in the dictionary
+    sortedvalues.append(item)       #appends each value in ascending order
+for item in wordlist.keys():        #loops through every single key in the dictionary
+    sortedkeys.append(item)         #appends each key according to the value of it in ascenfing order
 
     
-sortedkeys = sortedkeys[-1:-11:-1]
+sortedkeys = sortedkeys[-1:-11:-1] #takes the last 10 keys in the list, which has the highest value number for the respective keys
 
-sortedvalues = sortedvalues[-1:-11:-1]
+sortedvalues = sortedvalues[-1:-11:-1] #takes the last 10 values in the list, which is the highest in value number
 
-plt.plot(sortedkeys,sortedvalues,"o")
+plt.plot(sortedkeys,sortedvalues,"o")   #plots the graph with the keys in the x axis, values in the y axis, uses a dot to represent the values
 
-plt.show()
+plt.show()          #outputs the graph
 
