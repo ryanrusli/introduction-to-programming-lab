25970d73c6ceafc197b9983b874e51793140e785
ryanrusli1407 <43331671+ryanrusli1407@users.noreply.github.com>
2018-10-04 15:11:25 +0700

    Update ATM_Assignment.py

--- ATM_Assignment.py
+++ ATM_Assignment.py
@@ -7,34 +7,32 @@
 import random
 import os
 
-class ATM:
-    def __init__(self,ID,balance,wallet):
+class BankServices:     #class for all the function in a ATM
+    def __init__(self,ID,balance):
         self.ID = str(ID)
         self.balance = int(balance)
-        self.wallet = int(wallet)
-                 
-    def withdrawMoney(self,amount):
-        data = open("Accounts.txt", "r+")
+    def withdrawMoney(self,amount):     #removes money from the balance
+        data = open("Accounts.txt", "r+")       #opens the text file to be read and written
         lines = data.readlines()
         found = False
         pin = input("Please reconfirm with your pin: ")
-        for i in range(len(lines)-2):
+        for i in range(len(lines)-4):
             lines[i] = lines[i].rstrip("\n")
             lines[i+1] = lines[i+1].rstrip("\n")
             lines[i+2] = lines[i+2].rstrip("\n")
+            lines[i+3] = lines[i+3].rstrip("\n")
+            lines[i+4] = lines[i+4].rstrip("\n")
             if self.ID == lines[i]:
                 found = True
                 if self.balance > amount:
                     new = self.balance - amount
-                    self.wallet += amount
                     old = lines[i+2]
                     print("Your balance has been decreased to",new)
-                    print("Wallet =",self.wallet)
                     new = str(new)
                 elif amount > self.balance:
                     print("Insufficient balance!")
         data.seek(0)
-        data.truncate()
+        data.truncate()     #removes all the data in the txt file
         prevline =''
         for line in lines:
             if line == old and pin == prevline:
@@ -42,29 +40,26 @@ class ATM:
             else:
                 data.write(line+"\n")
             prevline = line
-
-    def depositMoney(self,amount):
-        data = open("Accounts.txt", "r+")
+    
+    def depositMoney(self,amount):              #adds money to the balance
+        data = open("Accounts.txt", "r+")       #opens the text file to be read and written
         lines = data.readlines()
         found = False
         pin = input("Please reconfirm with your pin: ")
-        for i in range(len(lines)-2):
+        for i in range(len(lines)-4):
             lines[i] = lines[i].rstrip("\n")
             lines[i+1] = lines[i+1].rstrip("\n")
             lines[i+2] = lines[i+2].rstrip("\n")
+            lines[i+3] = lines[i+3].rstrip("\n")
+            lines[i+4] = lines[i+4].rstrip("\n")
             if self.ID == lines[i]:
                 found = True
-                if self.wallet >= amount:
-                    new = int(lines[i+2]) + amount
-                    self.wallet -= amount
-                    old = lines[i+2]
-                    print("Your new balance is",new)
-                    new = str(new)
-                    print("Wallet:",self.wallet)
-                elif self.wallet < amount:
-                    print("Not enough cash!")
+                new = int(lines[i+2]) + amount
+                old = lines[i+2]
+                print("Your new balance is",new)
+                new = str(new)
         data.seek(0)
-        data.truncate()
+        data.truncate()             #removes all the data in the txt file
         prevline =''
         for line in lines:
             if line == old and pin == prevline:
@@ -73,15 +68,17 @@ class ATM:
                 data.write(line+"\n")
             prevline = line
             
-    def debit(self,amount):
-        data = open("Accounts.txt", "r+")
+    def debit(self,amount):                     #removes money from the account to debit
+        data = open("Accounts.txt", "r+")       #opens the text file to be read and written
         lines = data.readlines()
         found = False
         pin = input("Please reconfirm with your pin: ")
-        for i in range(len(lines)-2):
+        for i in range(len(lines)-4):
             lines[i] = lines[i].rstrip("\n")
             lines[i+1] = lines[i+1].rstrip("\n")
             lines[i+2] = lines[i+2].rstrip("\n")
+            lines[i+3] = lines[i+3].rstrip("\n")
+            lines[i+4] = lines[i+4].rstrip("\n")
             if self.ID == lines[i]:
                 found = True
                 if self.balance > amount:
@@ -92,7 +89,7 @@ class ATM:
                 elif amount > self.balance:
                     print("Insufficient balance!")
         data.seek(0)
-        data.truncate()
+        data.truncate()             #removes all the data in the txt file
         prevline =''
         for line in lines:
             if line == old and pin == prevline:
@@ -101,24 +98,52 @@ class ATM:
                 data.write(line+"\n")
             prevline = line
 
-    def TransferMoney(self,transferID):
-        data = open("Accounts.txt", "r+")
+    def TransferMoney(self,login,transferID):         #transfers money to another function
+        data = open("Accounts.txt", "r+")       #opens the text file to be read and written
         lines = data.readlines()
         found = False
-        for line in range(len(data)):
-            if transferID == line:
+        for i in range(len(lines)-4):
+            lines[i] = lines[i].rstrip("\n")
+            lines[i+1] = lines[i+1].rstrip("\n")
+            lines[i+2] = lines[i+2].rstrip("\n")
+            lines[i+3] = lines[i+3].rstrip("\n")
+            lines[i+4] = lines[i+4].rstrip("\n")
+            if transferID == lines[i]:
                 amount = int(input("Input the amount you would like to transfer: "))
-                if amount >=0:
+                pin = input("Input pin to confirm")
+                if amount > =0 and line[i+2]>amount:
                     found = True
-                    data[line+2] += amount
-                    print("Amount successfully transferred.")
+                    transfer_oldbalance = int(lines[i+2])
+                    transfer_newbalance = transfer_oldbalance + amount
+                    transferee = lines[i+3]
+                    transfer_newbalance = str(transfer_newbalance)
+                    transfer_oldbalance = str(transfer_oldbalance)
+                    print("Amount successfully transferred to",transferee,".")
                 else:
-                    print("Invalid amount!")
+                    print("Insufficient amount or invalid amount!")
+            elif login == lines[i]:
+                my_oldbalance = int(lines[i+2])
+                my_newbalance = my_oldbalance - amount
+                my_oldbalance = str(my_oldbalance)
+                my_newbalance = str(my_newbalance)
         if found == False:
             print("Account ID not found")
-
-class Account:        
-    def createAccount(self):
+        elif found ==True:
+            data.seek(0)
+            data.truncate()         #removes all the data in the txt file
+            prevline =''
+            for line in lines:
+                if line == my_oldbalance and prevline == pin:
+                    data.write(my_newbalance + "\n")
+                elif line == transfer_oldbalance:
+                    data.write(line.replace(transfer_oldbalance,transfer_newbalance)+ "\n")
+                else:
+                    data.write(line + "\n")
+                prevline = line
+                               
+                    
+class Account:                  #Account Class
+    def createAccount(self):    #function to create account, and manage logins
         newID = ''
         count = 0
         balance = 0
@@ -126,6 +151,8 @@ class Account:
             newID += str(random.randint(0,9))
             count += 1
         newID = int(newID)
+        firstname = input("Please input your first name: ")
+        lastname = input("Please input your last name: ")
         print("Your account ID is",newID,"\nPlease note it down.")
         confirmed = False
         while confirmed == False:
@@ -135,35 +162,38 @@ class Account:
                 confirmed = True
                 print("Pins are the same.")
                 print("Account creation success!")
+                print("Thank you for registering,",firstname)
                 pin = int(pin)
             else:
                 print("Pins are not the same, please recheck")
-        return newID,pin,balance
+        return newID,pin,balance,firstname,lastname
 
-    def checkcard(self,login):
+    def checkcard(self,login):      #checks login credentials
         data = open("Accounts.txt", "rt")
         lines = data.readlines()
         found = False
-        for i in range(len(lines)):
+        for i in range(len(lines)-4):
             lines[i] = lines[i].rstrip("\n")
             lines[i+1] = lines[i+1].rstrip("\n")
             lines[i+2] = lines[i+2].rstrip("\n")
+            lines[i+3] = lines[i+3].rstrip("\n")
+            lines[i+4] = lines[i+4].rstrip("\n")
             if login == lines[i] :
                 found = True
                 pin = str(input("Input your pin: "))
                 if pin == lines[i+1]:
                     print("Login successs!")
-                    print(lines[i+1])
+                    print("Good day,", lines[i+3])
                     return True
                     break
                 else:
                     print("Incorrect PIN")
-                    print(lines[i+1])
+                    break
         if found == False:
             print("Account ID not found.")
             return False
                     
-    def getBalance(self,login):
+    def getBalance(self,login):     #collects the balance
         data = open("Accounts.txt", "rt")
         data = data.read()
         data = data.split()
@@ -173,27 +203,27 @@ class Account:
                 balance = data[line+2]
                 return balance
 
-def main():
+def main():     #main function
     f = open("Accounts.txt" , "a+")
     x = input("Is this your first time using this system? [y/n] ")
     start = Account()
     if x == "y":
         newAccount = start.createAccount()
-        f.flush()
-        os.fsync(f.fileno())
         for i in newAccount:           
             f.write("%s\n"%i)
+
+        f.flush()
+        os.fsync(f.fileno())            #these two lines removes the buffer in python and the OS, and instantly updates the data
         
     
     login = str(input("Input your login id: "))
     
-    
-    checked = start.checkcard(login)
+    checked = start.checkcard(login)                    #checks login credentials, returns boolean value
+
     if checked:
         balance = start.getBalance(login)
-        wallet = int(input("Input wallet amount: "))
-        service = ATM(login,balance,wallet)
-        option = int(input("1.Withdraw\n2.Deposit\n3.Debit\n4.Transfer\n"))
+        service = BankServices(login,balance)
+        option = int(input("1.Withdraw\n2.Deposit\n3.Debit\n4.Transfer\n"))     #services in the bank
         if option == 1:
             amount = int(input("Input withdraw amount: "))
             service.withdrawMoney(amount)
@@ -205,14 +235,14 @@ def main():
             service.debit(amount)
         elif option == 4:
             transferID = input("Input the ID you are transferring to: ")
-            TransferMoney(transferID)
+            service.TransferMoney(login,transferID)
 
     
             
 
     f.flush()
-    os.fsync(f.fileno())
-    f.close()
+    os.fsync(f.fileno())            #these two lines removes the buffer in python and the OS, and instantly updates the data
+    f.close()                       #closes the file
     
     
         
